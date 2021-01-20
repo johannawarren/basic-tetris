c6d4d581277145ae0a856a2fda168a1f80c0cec4
jotothehanna <54164602+Jotothehanna@users.noreply.github.com>
2021-01-19 21:58:09 -0700

    second commit

---
+++ app.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const scoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
+  const width = 10
+
+  console.log(squares)
+
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+  const zTetromino = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+
+  const oTetromino = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const iTetromino = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+  let currentPosition = 4
+  let current = theTetrominoes[0][0]
+
+  //randomly select a Tetromino and it's first rotation
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+  let current = theTetrominoes[0][0]
+//draw the first rotation
+function draw() {
+  current.forEach(index => {
+    squares[currentPosition + index].classList.add('tetromino')
+  })
+}
+
+draw()
+
+
+
+
+
+
+
+
+
+
+
+})
